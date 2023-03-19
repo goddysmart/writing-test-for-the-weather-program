e383dfa1c7a385da50ccc915cce8ebc29fab8712
thegoddysmart <75092165+goddysmart@users.noreply.github.com>
2023-03-19 07:51:29 +0000

    modified the `show_weather_to_user` function
    Updated the version of the show_weather_to_user function that returns a string instead of using print:

--- program.py
+++ program.py
@@ -7,20 +7,21 @@ map_city_to_coords = {
     'Accra': 'lat=5.63&lon=0.39W'
 }
 
-# showing weather in a more friendly way
+# showing weather in a more friendly way - updated version
 def show_weather_to_user(weather_data_list):
+    output_str = ""
     for weather_data in weather_data_list:
         hour_number = weather_data['timepoint']
         temperature = weather_data['temp2m']
-        print(f'On day {hour_number},')
+        output_str += f'On day {hour_number},\n'
         if hour_number == 24:
-            print('(in one day)')
+            output_str += '(in one day)\n'
         elif hour_number == 48:
-            print('(in two days)')
+            output_str += '(in two days)\n'
         elif hour_number == 72:
-            print('(in three days)')
-            
-        print(f'The temperature is {temperature}')
+            output_str += '(in three days)\n'
+        output_str += f'The temperature is {temperature}\n'
+    return output_str
 
 def show_weather():
     
@@ -52,13 +53,6 @@ show_weather()
         
 
 
-# Getting the new coordinated format
-
-# This is the initial program. The walkthrough shows how to modify this code.
-# At the end of the walkthrough, this code will read and write to a json file.
-#
-# You can see the completed code after the walkthrough by going here,
-# https://github.com/kibo-programming-2-jan-23/walkthroughs/tree/main/show-weather-from-file/end
-
-## Change the current city
-# At the end of the program, ask the user if they would like to change the current city. If they type yes, let them type in a city, and then store that as the current city.
+# testing output `show_weather_to_user` without actually printing to the console
+output_str = show_weather_to_user(weather_data_list)
+print(output_str) 
